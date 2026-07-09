d126b188acedf4e5cec66308839bb1d3de60f145
callisto1337 <hazardous333@gmail.com>
2026-07-09 16:35:58 +0300

    removed daily summary

--- app/bot/runner.py
+++ app/bot/runner.py
@@ -12,7 +12,6 @@ from app.bootstrap import bootstrap_app
 
 from .callbacks import handle_rating_callback
 from .commands import report_command
-# from .daily_summary import job as daily_summary_job
 
 bootstrap_app()
 
@@ -40,16 +39,6 @@ def run_bot():
     )
 
     print("🤖 Бот запущен", flush=True)
-
-    # try:
-    #     # Moscow time = UTC+3
-    #     msk_tz = timezone(timedelta(hours=3))
-    #     app.job_queue.run_daily(
-    #         daily_summary_job,
-    #         time=time(hour=0, minute=0, tzinfo=msk_tz)
-    #     )
-    # except Exception as e:
-    #     print(f"Не удалось зарегистрировать ежедневную сводку: {e}", flush=True)
 
     app.run_polling()
 
